21a21534f9044472f9dd5d5394f40db8f30cf219
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-09 20:36:25 +0600

    basic navigation added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from "reactstrap";
+import logo from "../../assests/logo.png";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div
+      className="container-fluid mb-3"
+      style={{ backgroundColor: "#D70F64" }}
+    >
+      <Navbar>
+        <NavbarBrand className="nav_logo" href="/">
+          <img src={logo} alt="logo" />
+        </NavbarBrand>
+        <Nav className="ml-auto">
+          <NavItem>
+            <NavLink className="nav_link" href="#">
+              Something
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
     </div>
   );
 };

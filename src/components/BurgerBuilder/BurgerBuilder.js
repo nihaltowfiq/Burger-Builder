af0fbd2f5ab0126d8de79a3bbeece5b78d698ff5
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-09 19:45:58 +0600

    initial setup

---
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Burger Builder</h3>
+      </div>
+    );
+  }
+}
+
+export default BurgerBuilder;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BurgerBuilder from "./BurgerBuilder/BurgerBuilder";
+import Header from "./Header/Header";
+
+const Main = (props) => {
+  return (
+    <div>
+      <Header />
+      <BurgerBuilder />
+    </div>
+  );
+};
+
+export default Main;

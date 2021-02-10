b730d42cfbe344a3800133999213b12f8ad8820d
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-10 17:38:55 +0600

    static ingredient functionality done

--- src/components/BurgerBuilder/Burger.js
+++ src/components/BurgerBuilder/Burger.js
@@ -1,13 +1,27 @@
 import React from "react";
+import "./Burger.css";
 import Ingredient from "./Ingredient";
 
-const Burger = () => {
+const Burger = ({ ingredients }) => {
+  let ingredientArr = ingredients
+    .map((item) => {
+      let amountArr = [...Array(item.amount).keys()];
+      return amountArr.map(() => {
+        return <Ingredient type={item.type} key={Math.random()} />;
+      });
+    })
+    .reduce((arr, element) => {
+      return arr.concat(element);
+    }, []);
+
+  if (ingredientArr.length === 0) {
+    ingredientArr = <p>Please add some ingredients</p>;
+  }
+
   return (
-    <div className="my-4">
+    <div className="my-4 burger">
       <Ingredient type="bread_top" />
-      <Ingredient type="cheese" />
-      <Ingredient type="salad" />
-      <Ingredient type="meat" />
+      {ingredientArr}
       <Ingredient type="bread_bottom" />
     </div>
   );
--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import Burger from "./Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: [
+      { type: "cheese", amount: 2 },
+      { type: "salad", amount: 1 },
+      { type: "meat", amount: 2 },
+    ],
+  };
   render() {
     return (
       <div>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
       </div>
     );
   }

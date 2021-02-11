5e9b587240c10228f91342e359d0aeeffb20fd76
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-11 16:07:18 +0600

    removeIngredientHandle done

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -5,14 +5,13 @@ import Controls from "./Controls";
 class BurgerBuilder extends Component {
   state = {
     ingredients: [
-      { type: "cheese", amount: 2 },
-      { type: "salad", amount: 1 },
-      { type: "meat", amount: 2 },
+      { type: "cheese", amount: 0 },
+      { type: "salad", amount: 0 },
+      { type: "meat", amount: 0 },
     ],
   };
 
   addIngredientHandle = (type) => {
-    console.log(type);
     const newIngredients = [...this.state.ingredients];
     for (let item of newIngredients) {
       if (item.type === type) {
@@ -24,14 +23,25 @@ class BurgerBuilder extends Component {
     // Below the code in my other thought
     // let matchType = this.state.ingredients.find((item) => item.type === type);
     // matchType.amount++;
-    // this.setState({
-    //   ...this.state.ingredients,
-    //   matchType,
-    // });
+    // this.setState({ ...this.state.ingredients, matchType });
   };
 
   removeIngredientHandle = (type) => {
-    console.log(type);
+    const newIngredients = [...this.state.ingredients];
+    for (let item of newIngredients) {
+      if (item.type === type) {
+        if (item.amount <= 0) return;
+        item.amount--;
+      }
+    }
+    this.setState({ ingredients: newIngredients });
+
+    // Below the code in my other thought
+    // let matchType = this.state.ingredients.find((item) => item.type === type);
+    // if (matchType.amount !== 0) {
+    //   matchType.amount--;
+    // }
+    // this.setState({ ...this.state.ingredients, matchType });
   };
   render() {
     return (

724fe90ed0ffe89c0ad9c63edc5533a037430741
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-13 21:57:00 +0600

    spinner added to orders

--- src/components/BurgerBuilder/Orders/Orders.js
+++ src/components/BurgerBuilder/Orders/Orders.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchOrders } from "../../../redux/actionsCreator";
+import Spinner from "../../Spinner/Spinner";
 import Order from "./Order";
 
 const Orders = ({ fetchOrders, orders, orderLoading, orderError }) => {
   useEffect(() => fetchOrders(), [fetchOrders]);
   const order = orders.map((order) => <Order key={order.id} order={order} />);
-  return <div>{order}</div>;
+  return <div>{orderLoading ? <Spinner /> : order}</div>;
 };
 
 const mapStateToProps = (state) => {

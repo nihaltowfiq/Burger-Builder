be5a78dfd5bfed127991532aff856528fb8ff711
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-13 20:11:37 +0600

    redux-thunk added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import { Reducer } from "./reducer";
 
-export const store = createStore(Reducer);
+export const store = createStore(Reducer, applyMiddleware(thunk));

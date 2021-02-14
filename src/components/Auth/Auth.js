5794094b9ef165f160ab5ac64dd615dcd4bdd30a
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-14 12:58:31 +0600

    switching Sign Up to Login btn

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik } from "formik";
 
 const formStyle = {
@@ -8,6 +8,11 @@ const formStyle = {
 };
 
 const Auth = () => {
+  const [mode, setMode] = useState("Sign Up");
+
+  const switchModeHandler = () => {
+    setMode(mode === "Sign Up" ? "Login" : "Sign Up");
+  };
   return (
     <div>
       <Formik
@@ -29,18 +34,26 @@ const Auth = () => {
             errors.password = "Must be atleast 4 characters";
           }
 
-          if (!values.confirmPassword) {
-            errors.confirmPassword = "Required";
-          } else if (values.password !== values.confirmPassword) {
-            errors.confirmPassword = "Password field doesn't match!";
+          if (mode === "Sign Up") {
+            if (!values.confirmPassword) {
+              errors.confirmPassword = "Required";
+            } else if (values.password !== values.confirmPassword) {
+              errors.confirmPassword = "Password field doesn't match!";
+            }
           }
           //   console.log("ERRORS:", errors);
           return errors;
         }}
       >
         {({ values, handleChange, handleSubmit, errors }) => (
-          <div>
-            <form style={formStyle} onSubmit={handleSubmit}>
+          <div style={formStyle}>
+            <button
+              className="btn btn-lg btn-pink btn-block mb-3"
+              onClick={switchModeHandler}
+            >
+              Switch to {mode === "Sign Up" ? "Login" : "Sign Up"}
+            </button>
+            <form onSubmit={handleSubmit}>
               <input
                 name="email"
                 placeholder="Enter Your Email"
@@ -61,18 +74,22 @@ const Auth = () => {
               />
               <span className="text-danger">{errors.password}</span>
               <br />
-              <input
-                name="confirmPassword"
-                placeholder="Confirm Password"
-                className="form-control"
-                type="password"
-                value={values.confirmPassword}
-                onChange={handleChange}
-              />
-              <span className="text-danger">{errors.confirmPassword}</span>
-              <br />
+              {mode === "Sign Up" && (
+                <>
+                  <input
+                    name="confirmPassword"
+                    placeholder="Confirm Password"
+                    className="form-control"
+                    type="password"
+                    value={values.confirmPassword}
+                    onChange={handleChange}
+                  />
+                  <span className="text-danger">{errors.confirmPassword}</span>
+                  <br />
+                </>
+              )}
               <button type="submit" className="btn btn-success">
-                Sign Up
+                {mode === "Sign Up" ? "Sign Up" : "Login"}
               </button>
             </form>
           </div>

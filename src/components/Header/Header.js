67ed39405533e88472d03b4a78a503c4795cf188
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-14 22:01:00 +0600

    conditional navs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,39 @@
 import React from "react";
+import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { Nav, Navbar, NavbarBrand, NavItem } from "reactstrap";
 import logo from "../../assests/logo.png";
 import "./Header.css";
 
-const Header = () => {
+const Header = ({ email }) => {
+  let links = null;
+  if (email === null) {
+    links = (
+      <Nav className="ml-auto">
+        <NavItem>
+          <NavLink className="nav_link" exact to="/login">
+            Login
+          </NavLink>
+        </NavItem>
+      </Nav>
+    );
+  } else {
+    links = (
+      <Nav className="ml-auto">
+        <NavItem>
+          <NavLink className="nav_link" exact to="/">
+            Burger Builder
+          </NavLink>
+          <NavLink className="nav_link" exact to="/orders">
+            Orders
+          </NavLink>
+          <NavLink className="nav_link" exact to="/login">
+            Logout
+          </NavLink>
+        </NavItem>
+      </Nav>
+    );
+  }
   return (
     <div
       className="container-fluid mb-3"
@@ -14,22 +43,17 @@ const Header = () => {
         <NavbarBrand className="nav_logo" href="/">
           <img src={logo} alt="logo" />
         </NavbarBrand>
-        <Nav className="ml-auto">
-          <NavItem>
-            <NavLink className="nav_link" exact to="/">
-              Burger Builder
-            </NavLink>
-            <NavLink className="nav_link" exact to="/orders">
-              Orders
-            </NavLink>
-            <NavLink className="nav_link" exact to="/login">
-              Login
-            </NavLink>
-          </NavItem>
-        </Nav>
+
+        {links}
       </Navbar>
     </div>
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    email: state.email,
+  };
+};
+
+export default connect(mapStateToProps)(Header);
